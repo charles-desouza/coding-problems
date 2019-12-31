52ffead2f0c2d0816b8a6c66527d624be08d00b2
charles-desouza <charles5388@gmail.com>
2019-12-31 16:23:50 +0530

    Added string reverse

---
+++ StringReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringReverse
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = "abcd";
+
+      Console.WriteLine("Input = " + input);
+      Console.WriteLine("Reverse = " + input.Reverse());
+    }
+  }
+}
---
+++ StringReverse/StringUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StringReverse
+{
+  public static class StringUtility
+  {
+    public static string Reverse(this string input)
+    {
+      if(input.Equals(string.Empty))
+        return input;
+
+      int n = input.Length -1;
+      char[] str = input.ToCharArray();
+
+      for (int i = 0; i <= n / 2; i++)
+      {
+        char tmp = str[i];
+        str[i] = str[n-i];
+        str[n - i] = tmp;
+      }
+
+      return new string(str);
+    }
+  }
+}
