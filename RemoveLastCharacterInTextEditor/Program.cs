f13accd65e4ffa82c9a525b02fb712014e69c350
charles-desouza <charles5388@gmail.com>
2020-01-20 12:19:25 +0530

    Remove last character in text editor

---
+++ RemoveLastCharacterInTextEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RemoveLastCharacterInTextEditor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TextEditor editor = new TextEditor();
+      editor.Write('H');
+      editor.Write('E');
+      editor.Write('L');
+      editor.Write('L');
+      editor.Write('o');
+      editor.Write('o');
+      Console.WriteLine(editor.ToString());
+      editor.Undo();
+      Console.WriteLine(editor.ToString());
+
+      Console.WriteLine();
+    }
+  }
+}
---
+++ RemoveLastCharacterInTextEditor/TextEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace RemoveLastCharacterInTextEditor
+{
+  public class TextEditor
+  {
+    private readonly StringBuilder sb;
+    public TextEditor()
+    {
+      sb = new StringBuilder();
+    }
+
+    public void Write(char word)
+    {
+      sb.Append(word);
+    }
+    public void Undo()
+    {
+      sb.Length--;
+    }
+
+    public override string ToString()
+    {
+      return sb.ToString();
+    }
+  }
+}
